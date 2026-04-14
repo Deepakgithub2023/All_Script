e173e9a691de80dfa8a4cfa299fdb8f3bd7fe0cb
Deepakgithub2023 <147323372+Deepakgithub2023@users.noreply.github.com>
2026-04-14 22:46:50 +0530

    Create my_first_script.sh for system info
    Initial script to provide system information including Apache processes, memory usage, and number of processors.

---
+++ my_first_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#######################################################
+
+# this is my first script file
+
+# this script give you information about system
+
+# author - Deepak
+
+# command to run this script file
+
+#  sh <file_name>
+
+#my_first_script.sh
+
+
+#######################################################
+
+set -x #Seprate with the command before the output
+set -e #Exit script when there is an error
+set -o pipefail # it will stop if the pipe is not getting response
+
+ps -ef | grep "apache" | awk -F " " '{print $2}'
+
+free
+
+nproc
+
+ps -ef | grep httpd
